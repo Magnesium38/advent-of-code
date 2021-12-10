6bfd1df7c460cd68c25ca06365f0928c1416becf
Eric Jensen <magnesium38@gmail.com>
2021-12-10 07:15:59 -0600

    extract common functionality

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -7,29 +7,7 @@ fn pt1(input: &str) -> anyhow::Result<isize> {
 			let mut stack = Vec::new();
 
 			line.chars()
-				.find_map(|c| match c {
-					'(' | '[' | '{' | '<' => {
-						stack.push(c);
-						None
-					}
-
-					_ => match (stack.pop(), c) {
-						(Some('('), ')')
-						| (Some('['), ']')
-						| (Some('{'), '}')
-						| (Some('<'), '>') => None,
-
-						(Some(_), _) => match c {
-							')' => return Some(3),
-							']' => return Some(57),
-							'}' => return Some(1197),
-							'>' => return Some(25137),
-							_ => unreachable!(),
-						},
-
-						(None, _) => unreachable!(),
-					},
-				})
+				.find_map(|c| handle_character(&mut stack, c))
 				.unwrap_or(0)
 		})
 		.sum())
@@ -41,47 +19,51 @@ fn pt2(input: &str) -> anyhow::Result<isize> {
 		.map(|line| {
 			let mut stack = Vec::new();
 
-			let is_corrupted = line.chars().find_map(|c| match c {
-				'(' | '[' | '{' | '<' => {
-					stack.push(c);
-					None
+			match line.chars().find_map(|c| handle_character(&mut stack, c)) {
+				Some(_) => 0,
+				None => {
+					stack.reverse();
+					stack.iter().fold(0, |acc, c| {
+						(acc * 5)
+							+ match c {
+								'(' => 1,
+								'[' => 2,
+								'{' => 3,
+								'<' => 4,
+								_ => unreachable!(),
+							}
+					})
 				}
-
-				_ => match (stack.pop(), c) {
-					(Some('('), ')') | (Some('['), ']') | (Some('{'), '}') | (Some('<'), '>') => {
-						None
-					}
-
-					(Some(_), _) => match c {
-						')' | ']' | '}' | '>' => return Some(true),
-						_ => unreachable!(),
-					},
-
-					(None, _) => unreachable!(),
-				},
-			});
-
-			if let Some(true) = is_corrupted {
-				return 0;
 			}
-
-			stack.reverse();
-			stack.iter().fold(0, |acc, c| {
-				(acc * 5)
-					+ match c {
-						'(' => 1,
-						'[' => 2,
-						'{' => 3,
-						'<' => 4,
-						_ => 0,
-					}
-			})
 		})
 		.filter(|&score| score > 0)
 		.sorted()
-		.collect::<Vec<_>>();
+		.collect_vec();
 
 	Ok(scores[scores.len() / 2])
+}
+
+fn handle_character(stack: &mut Vec<char>, c: char) -> Option<isize> {
+	match c {
+		'(' | '[' | '{' | '<' => {
+			stack.push(c);
+			None
+		}
+
+		_ => match (stack.pop(), c) {
+			(Some('('), ')') | (Some('['), ']') | (Some('{'), '}') | (Some('<'), '>') => None,
+
+			(Some(_), _) => match c {
+				')' => Some(3),
+				']' => Some(57),
+				'}' => Some(1197),
+				'>' => Some(25137),
+				_ => unreachable!(),
+			},
+
+			(None, _) => unreachable!(),
+		},
+	}
 }
 
 advent::problem!(
