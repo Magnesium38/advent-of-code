5d616ee56cbb34111d75753ad09b190cc0620e35
Eric Jensen <magnesium38@gmail.com>
2021-12-12 01:26:50 -0600

    use shared function

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -3,75 +3,51 @@ use std::collections::{HashMap, HashSet};
 use itertools::Itertools;
 
 fn pt1(input: &str) -> anyhow::Result<usize> {
-	fn count_paths(
-		starting_point: &Cave,
-		mapping: &HashMap<&str, Vec<Cave>>,
-		visited: &HashSet<&str>,
-	) -> usize {
-		if starting_point.s == "end" {
-			return 1;
-		}
-
-		let is_lowercase = starting_point.lowercase;
-		if visited.contains(starting_point.s) && is_lowercase {
-			return 0;
-		}
-
-		let mut visited = visited.clone();
-		if is_lowercase {
-			visited.insert(starting_point.s);
-		}
-
-		mapping[starting_point.s]
-			.iter()
-			.map(|next_point| count_paths(next_point, mapping, &visited))
-			.sum()
-	}
-
 	Ok(count_paths(
 		&Cave::from("start"),
 		&build_graph(input),
 		&HashSet::new(),
+		true,
 	))
 }
 
 fn pt2(input: &str) -> anyhow::Result<usize> {
-	fn count_paths(
-		starting_point: &Cave,
-		mapping: &HashMap<&str, Vec<Cave>>,
-		visited: &HashSet<&str>,
-		mut has_double_visited: bool,
-	) -> usize {
-		if starting_point.s == "end" {
-			return 1;
-		}
-
-		let is_lowercase = starting_point.lowercase;
-		if is_lowercase && has_double_visited && visited.contains(starting_point.s) {
-			return 0;
-		}
-
-		let mut visited = visited.clone();
-		if is_lowercase && visited.contains(starting_point.s) {
-			has_double_visited = true;
-		}
-
-		if is_lowercase {
-			visited.insert(starting_point.s);
-		}
-
-		mapping[starting_point.s]
-			.iter()
-			.map(|next_point| count_paths(next_point, mapping, &visited, has_double_visited))
-			.sum()
-	}
-
 	Ok(count_paths(
 		&Cave::from("start"),
 		&build_graph(input),
 		&HashSet::new(),
 		false,
 	))
+}
+
+fn count_paths(
+	starting_point: &Cave,
+	mapping: &HashMap<&str, Vec<Cave>>,
+	visited: &HashSet<&str>,
+	mut has_double_visited: bool,
+) -> usize {
+	if starting_point.s == "end" {
+		return 1;
+	}
+
+	let is_lowercase = starting_point.lowercase;
+	if is_lowercase && has_double_visited && visited.contains(starting_point.s) {
+		return 0;
+	}
+
+	let mut visited = visited.clone();
+	if is_lowercase && visited.contains(starting_point.s) {
+		has_double_visited = true;
+	}
+
+	if is_lowercase {
+		visited.insert(starting_point.s);
+	}
+
+	mapping[starting_point.s]
+		.iter()
+		.map(|next_point| count_paths(next_point, mapping, &visited, has_double_visited))
+		.sum()
 }
 
 fn build_graph(input: &str) -> HashMap<&str, Vec<Cave>> {
