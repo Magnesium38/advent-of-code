006943554b31b1ba0fe85b39fff7042a38316f99
Eric Jensen <magnesium38@gmail.com>
2021-12-12 09:56:31 -0600

    don't use renamed variable

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -23,30 +23,29 @@ const START: u8 = 0;
 const END: u8 = 1;
 
 fn count_paths(
-	starting_point: &Cave,
+	cave: &Cave,
 	mapping: &HashMap<u8, Vec<Cave>>,
 	visited: &HashSet<u8>,
 	mut has_double_visited: bool,
 ) -> usize {
-	if starting_point.id == END {
+	if cave.id == END {
 		return 1;
 	}
 
-	let is_lowercase = starting_point.is_small;
-	if is_lowercase && has_double_visited && visited.contains(&starting_point.id) {
+	if cave.is_small && has_double_visited && visited.contains(&cave.id) {
 		return 0;
 	}
 
 	let mut visited = visited.clone();
-	if is_lowercase && visited.contains(&starting_point.id) {
+	if cave.is_small && visited.contains(&cave.id) {
 		has_double_visited = true;
 	}
 
-	if is_lowercase {
-		visited.insert(starting_point.id);
+	if cave.is_small {
+		visited.insert(cave.id);
 	}
 
-	mapping[&starting_point.id]
+	mapping[&cave.id]
 		.iter()
 		.map(|next_point| count_paths(next_point, mapping, &visited, has_double_visited))
 		.sum()
