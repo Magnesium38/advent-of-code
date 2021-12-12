9f9e6ec83f998122fef25b65934762a59f0469c6
Eric Jensen <magnesium38@gmail.com>
2021-12-12 00:31:11 -0600

    build graph in part two

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -41,79 +41,60 @@ fn pt2(input: &str) -> anyhow::Result<usize> {
 	) -> Vec<String> {
 		let mut paths: Vec<String> = Vec::new();
 
-		mapping
-			.clone()
-			.get(starting_point)
-			.unwrap()
-			.iter()
-			.for_each(|end| {
-				if end == &"start" {
-					return;
-				}
+		mapping[starting_point].iter().for_each(|end| {
+			if end == &"start" {
+				return;
+			}
 
-				if end == &"end" {
-					let mut dest = path.clone();
-					dest.push_str(format!(",{}", end).as_str());
-					paths.push(dest);
-					return;
-				}
+			if end == &"end" {
+				let mut dest = path.clone();
+				dest.push_str(format!(",{}", end).as_str());
+				paths.push(dest);
+				return;
+			}
 
-				let mut new_path = path.clone();
-				new_path.push_str(format!(",{}", end).as_str());
+			let mut new_path = path.clone();
+			new_path.push_str(format!(",{}", end).as_str());
 
-				let mut visited = visited.clone();
-				if end.chars().all(|c| c.is_ascii_lowercase()) {
-					if visited.contains(end) {
-						if has_double_visited {
-							return;
-						}
-
-						let mut visited = visited.clone();
-						visited.insert(end);
-						paths.extend(find_paths(
-							end,
-							mapping.clone(),
-							new_path.clone(),
-							visited,
-							true,
-						));
+			let mut visited = visited.clone();
+			if end.chars().all(|c| c.is_ascii_lowercase()) {
+				if visited.contains(end) {
+					if has_double_visited {
 						return;
 					}
 
+					let mut visited = visited.clone();
 					visited.insert(end);
+					paths.extend(find_paths(
+						end,
+						mapping.clone(),
+						new_path.clone(),
+						visited,
+						true,
+					));
+					return;
 				}
 
-				paths.extend(find_paths(
-					end,
-					mapping.clone(),
-					new_path,
-					visited,
-					has_double_visited,
-				));
-			});
+				visited.insert(end);
+			}
+
+			paths.extend(find_paths(
+				end,
+				mapping.clone(),
+				new_path,
+				visited,
+				has_double_visited,
+			));
+		});
 
 		paths
 	}
 
-	let mut mapping = HashMap::new();
-
-	input.lines().for_each(|line| {
-		let (start, end) = line.split('-').take(2).collect_tuple().unwrap();
-
-		{
-			mapping.entry(start).or_insert_with(Vec::new).push(end);
-		}
-
-		{
-			mapping.entry(end).or_insert_with(Vec::new).push(start);
-		}
-	});
-
 	let paths = find_paths(
 		"start",
-		mapping.clone(),
+		build_graph(input),
 		String::from("start"),
-		HashSet::from_iter(vec!["start"]),
+		HashSet::new(),
 		false,
 	);
 
