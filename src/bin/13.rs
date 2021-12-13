2d83e1481ded30642b53da6f325cb5a580ee1aec
Eric Jensen <magnesium38@gmail.com>
2021-12-13 08:40:10 -0600

    extract helper functions to use in pt 1

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,75 +1,25 @@
 use hashbrown::HashSet;
 
+type Point = (isize, isize);
+
 fn pt1(input: &str) -> anyhow::Result<usize> {
 	let (points, folds) = input.split_once("\n\n").unwrap();
 
-	let mut grid: HashSet<(isize, isize)> = HashSet::new();
-	points.lines().for_each(|line| {
-		let (x, y) = line.split_once(',').unwrap();
-		let x = x.parse::<isize>().unwrap();
-		let y = y.parse::<isize>().unwrap();
+	let grid: HashSet<Point> = HashSet::from_iter(
+		folds
+			.lines()
+			.take(1)
+			.fold(initialize_points(points), apply_fold),
+	);
 
-		grid.insert((x, y));
-	});
-
-	let mut result = folds.lines().map(|line| {
-		let (_, amount) = line.split_once("=").unwrap();
-		let amount = amount.parse::<isize>().unwrap();
-
-		if line.contains("x=") {
-			grid.drain_filter(|&(x, _)| x > amount)
-				.collect::<Vec<_>>()
-				.iter()
-				.for_each(|&(x, y)| {
-					grid.insert((amount - (x - amount).abs(), y));
-				});
-		} else {
-			grid.drain_filter(|&(_, y)| y > amount)
-				.collect::<Vec<_>>()
-				.iter()
-				.for_each(|&(x, y)| {
-					grid.insert((x, amount - (y - amount).abs()));
-				});
-		}
-
-		grid.len()
-	});
-
-	Ok(result.next().unwrap())
+	Ok(grid.len())
 }
 
 fn pt2(input: &str) -> anyhow::Result<String> {
 	let (points, folds) = input.split_once("\n\n").unwrap();
 
-	let grid: HashSet<(isize, isize)> = HashSet::from_iter(
-		folds.lines().fold(
-			points
-				.lines()
-				.map(|line| {
-					let (x, y) = line.split_once(',').unwrap();
-					let x = x.parse::<isize>().unwrap();
-					let y = y.parse::<isize>().unwrap();
-
-					(x, y)
-				})
-				.collect::<Vec<_>>(),
-			|points, line| {
-				let is_x = line.contains("x=");
-				let (_, line) = line.split_once("=").unwrap();
-				let line = line.parse::<isize>().unwrap();
-
-				points
-					.iter()
-					.map(|&(x, y)| match (is_x, (x, y)) {
-						(true, (x, y)) if x > line => (line + line - x, y),
-						(true, (x, y)) => (x, y),
-						(false, (x, y)) if y > line => (x, line + line - y),
-						(false, (x, y)) => (x, y),
-					})
-					.collect()
-			},
-		),
-	);
+	let grid: HashSet<Point> =
+		HashSet::from_iter(folds.lines().fold(initialize_points(points), apply_fold));
 
 	let (max_x, _) = grid.iter().max_by_key(|(x, _)| *x).unwrap();
 	let (_, max_y) = grid.iter().max_by_key(|(_, y)| *y).unwrap();
@@ -88,6 +38,35 @@ fn pt2(input: &str) -> anyhow::Result<String> {
 	}
 
 	Ok(result)
+}
+
+fn initialize_points(input: &str) -> Vec<Point> {
+	input
+		.lines()
+		.map(|line| {
+			let (x, y) = line.split_once(',').unwrap();
+			let x = x.parse::<isize>().unwrap();
+			let y = y.parse::<isize>().unwrap();
+
+			(x, y)
+		})
+		.collect()
+}
+
+fn apply_fold(points: Vec<Point>, instruction: &str) -> Vec<Point> {
+	let is_x = instruction.contains("x=");
+	let (_, line) = instruction.split_once("=").unwrap();
+	let line = line.parse::<isize>().unwrap();
+
+	points
+		.iter()
+		.map(|&(x, y)| match (is_x, (x, y)) {
+			(true, (x, y)) if x > line => (line + line - x, y),
+			(true, (x, y)) => (x, y),
+			(false, (x, y)) if y > line => (x, line + line - y),
+			(false, (x, y)) => (x, y),
+		})
+		.collect()
 }
 
 advent::problem!(
