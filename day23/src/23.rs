09a338da994723dd6f92003eea4da689350bd9d2
Eric Jensen <magnesium38@gmail.com>
2021-12-23 22:24:10 -0600

    Essentially a full rewrite
    Cleaner logic so that duplicated cases are handled better. In particular, moves should be correctly visited only once, and Amphipods won't move into oscilate in and out of their rooms if it hasn't been emptied yet.

--- day23/src/23.rs
+++ day23/src/23.rs
@@ -4,305 +4,315 @@ use std::{
 	cmp::{Ordering, Reverse},
 	collections::BinaryHeap,
 	fmt::Display,
+	hash::Hash,
 };
 
 pub fn pt1(input: &str) -> anyhow::Result<usize> {
-	let mut state = State::new(2);
-	let mut visited = HashSet::new();
-	let mut queue = BinaryHeap::new();
-
-	input
-		.lines()
-		.skip(2)
-		.flat_map(|line| line.chars())
-		.filter(|&c| c == 'A' || c == 'B' || c == 'C' || c == 'D')
-		.map(|c| match c {
-			'A' => Occupant::A,
-			'B' => Occupant::B,
-			'C' => Occupant::C,
-			'D' => Occupant::D,
-			_ => unreachable!(),
-		})
-		.enumerate()
-		.for_each(|(i, c)| {
-			state.rooms[i % 4][if i >= 4 { 1 } else { 0 }] = c;
-		});
-
-	queue.push(state);
-
-	while let Some(state) = queue.pop() {
-		if !visited.insert(state.id()) {
-			continue;
-		}
-
-		if state.is_solution() {
-			return Ok(state.cost.0);
-		}
-
-		for new_state in state.moves() {
-			queue.push(new_state);
-		}
-	}
-
-	Err(anyhow::anyhow!("No solution found"))
+	solve(parse::<2>(input))
 }
 
 pub fn pt2(input: &str) -> anyhow::Result<usize> {
-	let mut state = State::new(4);
+	let mut initial_state = parse::<4>(input);
+
+	initial_state.rooms.iter_mut().for_each(|room| {
+		room.cells[3] = room.cells[1];
+	});
+
+	initial_state.rooms[0].cells[1] = Some(Amphipod::Desert);
+	initial_state.rooms[1].cells[1] = Some(Amphipod::Copper);
+	initial_state.rooms[2].cells[1] = Some(Amphipod::Bronze);
+	initial_state.rooms[3].cells[1] = Some(Amphipod::Amber);
+	initial_state.rooms[0].cells[2] = Some(Amphipod::Desert);
+	initial_state.rooms[1].cells[2] = Some(Amphipod::Bronze);
+	initial_state.rooms[2].cells[2] = Some(Amphipod::Amber);
+	initial_state.rooms[3].cells[2] = Some(Amphipod::Copper);
+
+	solve(initial_state)
+}
+
+fn parse<const N: usize>(input: &str) -> State<N> {
+	let input = input
+		.lines()
+		.skip(2)
+		.map(|line| {
+			line.chars()
+				.filter(|c| match c {
+					'A'..='D' => true,
+					_ => false,
+				})
+				.map(Amphipod::parse)
+				.collect_vec()
+		})
+		.collect_vec();
+
+	let mut rooms = [Room {
+		id: Amphipod::Amber,
+		cells: [None; N],
+	}; 4];
+
+	for i in 0..4 {
+		rooms[i].id = match i {
+			0 => Amphipod::Amber,
+			1 => Amphipod::Bronze,
+			2 => Amphipod::Copper,
+			3 => Amphipod::Desert,
+			_ => unreachable!(),
+		};
+
+		for j in 0..2 {
+			rooms[i].cells[j] = Some(input[j][i]);
+		}
+	}
+
+	State {
+		cost: 0,
+		rooms,
+		hallway: [None; 11],
+	}
+}
+
+fn solve<const N: usize>(initial_state: State<N>) -> anyhow::Result<usize> {
 	let mut visited = HashSet::new();
 	let mut queue = BinaryHeap::new();
 
-	input
-		.lines()
-		.skip(2)
-		.flat_map(|line| line.chars())
-		.filter(|&c| c == 'A' || c == 'B' || c == 'C' || c == 'D')
-		.map(|c| match c {
-			'A' => Occupant::A,
-			'B' => Occupant::B,
-			'C' => Occupant::C,
-			'D' => Occupant::D,
-			_ => unreachable!(),
-		})
-		.enumerate()
-		.for_each(|(i, c)| {
-			state.rooms[i % 4][if i >= 4 { 3 } else { 0 }] = c;
-		});
+	visited.insert(initial_state);
+	queue.push(Reverse(initial_state));
 
-	state.rooms[0][1] = Occupant::D;
-	state.rooms[1][1] = Occupant::C;
-	state.rooms[2][1] = Occupant::B;
-	state.rooms[3][1] = Occupant::A;
-	state.rooms[0][2] = Occupant::D;
-	state.rooms[1][2] = Occupant::B;
-	state.rooms[2][2] = Occupant::A;
-	state.rooms[3][2] = Occupant::C;
-
-	queue.push(state);
-
-	while let Some(state) = queue.pop() {
-		if !visited.insert(state.id()) {
-			continue;
+	while let Some(Reverse(state)) = queue.pop() {
+		if state.is_solved() {
+			return Ok(state.cost);
 		}
 
-		if state.is_solution() {
-			return Ok(state.cost.0);
-		}
-
-		for new_state in state.moves() {
-			queue.push(new_state);
+		for next_state in state.next_moves() {
+			if visited.insert(next_state) {
+				queue.push(Reverse(next_state));
+			}
 		}
 	}
 
 	Err(anyhow::anyhow!("No solution found"))
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-enum Occupant {
-	A,
-	B,
-	C,
-	D,
-	Empty,
+#[derive(PartialEq, Eq, Clone, Copy, Hash, Debug)]
+enum Amphipod {
+	Amber,
+	Bronze,
+	Copper,
+	Desert,
 }
 
-impl Display for Occupant {
+impl Amphipod {
+	fn cost(&self) -> usize {
+		match self {
+			Amphipod::Amber => 1,
+			Amphipod::Bronze => 10,
+			Amphipod::Copper => 100,
+			Amphipod::Desert => 1000,
+		}
+	}
+
+	fn destination(&self) -> usize {
+		match self {
+			Amphipod::Amber => 0,
+			Amphipod::Bronze => 1,
+			Amphipod::Copper => 2,
+			Amphipod::Desert => 3,
+		}
+	}
+
+	fn parse(c: char) -> Amphipod {
+		match c {
+			'A' => Amphipod::Amber,
+			'B' => Amphipod::Bronze,
+			'C' => Amphipod::Copper,
+			'D' => Amphipod::Desert,
+			_ => panic!("unexpected character: {}", c),
+		}
+	}
+}
+
+impl Display for Amphipod {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		match self {
-			Occupant::A => write!(f, "A"),
-			Occupant::B => write!(f, "B"),
-			Occupant::C => write!(f, "C"),
-			Occupant::D => write!(f, "D"),
-			Occupant::Empty => write!(f, "."),
+			Amphipod::Amber => write!(f, "A"),
+			Amphipod::Bronze => write!(f, "B"),
+			Amphipod::Copper => write!(f, "C"),
+			Amphipod::Desert => write!(f, "D"),
 		}
 	}
 }
 
-#[derive(Clone, Eq, PartialEq)]
-struct State {
-	cost: Reverse<usize>,
-	hallway: [Occupant; 11],
-	rooms: [Vec<Occupant>; 4],
+#[derive(Clone, Copy, Debug, Eq, Hash)]
+struct State<const N: usize> {
+	cost: usize,
+	hallway: [Option<Amphipod>; 11],
+	rooms: [Room<N>; 4],
 }
 
-impl std::fmt::Debug for State {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "\n{}", self.hallway.iter().join(""))?;
-
-		for i in 0..2 {
-			write!(f, "\n  ")?;
-			for j in 0..4 {
-				write!(f, "{} ", self.rooms[j][i])?;
-			}
-		}
-
-		write!(f, "\ncost: {}", self.cost.0)
+impl<const N: usize> PartialEq for State<N> {
+	fn eq(&self, other: &Self) -> bool {
+		self.hallway == other.hallway
+			&& self.rooms[0] == other.rooms[0]
+			&& self.rooms[1] == other.rooms[1]
+			&& self.rooms[2] == other.rooms[2]
+			&& self.rooms[3] == other.rooms[3]
 	}
 }
 
-impl PartialOrd for State {
+impl<const N: usize> PartialOrd for State<N> {
 	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
 		Some(self.cmp(other))
 	}
 }
 
-impl Ord for State {
+impl<const N: usize> Ord for State<N> {
 	fn cmp(&self, other: &Self) -> Ordering {
 		self.cost.cmp(&other.cost)
 	}
 }
 
-impl State {
-	fn new(n: usize) -> Self {
-		Self {
-			cost: Reverse(0),
-			hallway: [
-				Occupant::Empty,
-				Occupant::Empty,
-				Occupant::Empty,
-				Occupant::Empty,
-				Occupant::Empty,
-				Occupant::Empty,
-				Occupant::Empty,
-				Occupant::Empty,
-				Occupant::Empty,
-				Occupant::Empty,
-				Occupant::Empty,
-			],
-			rooms: [
-				vec![Occupant::Empty; n],
-				vec![Occupant::Empty; n],
-				vec![Occupant::Empty; n],
-				vec![Occupant::Empty; n],
-			],
+impl<const N: usize> State<N> {
+	fn is_solved(&self) -> bool {
+		self.rooms.iter().all(|room| room.is_solved())
+	}
+
+	fn cost_to_move_to_position(
+		&self,
+		from: usize,
+		to: usize,
+		amphipod: Amphipod,
+	) -> Option<usize> {
+		if from == to {
+			return None;
+		}
+
+		let (i1, i2) = if from < to {
+			(from + 1, to)
+		} else {
+			(to, from - 1)
+		};
+
+		if self.hallway[i1..=i2].iter().all(|cell| cell.is_none()) {
+			Some((i2 - i1 + 1) * amphipod.cost())
+		} else {
+			None
 		}
 	}
 
-	fn is_solution(&self) -> bool {
-		self.rooms[0].iter().all(|&c| c == Occupant::A)
-			&& self.rooms[1].iter().all(|&c| c == Occupant::B)
-			&& self.rooms[2].iter().all(|&c| c == Occupant::C)
-			&& self.rooms[3].iter().all(|&c| c == Occupant::D)
+	fn cost_to_move(&self, from: usize, amphipod: Amphipod) -> Option<usize> {
+		let to = 2 * amphipod.destination() + 2;
+
+		self.cost_to_move_to_position(from, to, amphipod)
 	}
 
-	fn id(&self) -> Vec<Occupant> {
-		self.hallway
-			.iter()
-			.chain(self.rooms.iter().flat_map(|r| r.iter()))
-			.copied()
-			.collect()
-	}
+	fn next_moves(&self) -> Vec<Self> {
+		let mut moves = vec![];
 
-	fn moves(&self) -> Vec<Self> {
-		let mut moves = Vec::new();
-
-		for (i, o) in self
-			.hallway
-			.iter()
-			.enumerate()
-			.filter(|(_, &c)| c != Occupant::Empty)
-		{
-			let room_index = match o {
-				Occupant::A => 0,
-				Occupant::B => 1,
-				Occupant::C => 2,
-				Occupant::D => 3,
-				_ => unreachable!(),
-			};
-
-			if let Some(room_position) = self.rooms[room_index]
-				.iter()
-				.positions(|&c| c == Occupant::Empty)
-				.last()
-			{
-				let ri = 2 + room_index * 2;
-				let (mut i1, mut i2) = (ri.min(i), ri.max(i));
-				if i1 == i {
-					i1 += 1;
-				} else {
-					i2 -= 1;
-				}
-				let can_move = self.hallway[i1..=i2].iter().all(|&c| c == Occupant::Empty);
-
-				if can_move {
-					let move_cost = i2 - i1 + 2 + room_position;
-
-					let mut new_state = self.clone();
-					new_state.cost = Reverse(
-						self.cost.0
-							+ match o {
-								Occupant::A => move_cost,
-								Occupant::B => move_cost * 10,
-								Occupant::C => move_cost * 100,
-								Occupant::D => move_cost * 1000,
-								_ => unreachable!(),
-							},
-					);
-
-					new_state.hallway[i] = Occupant::Empty;
-					new_state.rooms[room_index][room_position] = *o;
-
-					moves.push(new_state);
+		// Attempt to move hallway Amphipods into rooms
+		for hallway_index in 0..self.hallway.len() {
+			if let Some(amphipod) = self.hallway[hallway_index] {
+				if self.rooms[amphipod.destination()].accepts(amphipod) {
+					if let Some(hallway_cost) = self.cost_to_move(hallway_index, amphipod) {
+						let mut next_state = *self;
+						let room_cost = next_state.rooms[amphipod.destination()].insert(amphipod);
+						next_state.hallway[hallway_index] = None;
+						next_state.cost += hallway_cost + room_cost;
+						moves.push(next_state);
+					}
 				}
 			}
 		}
 
-		for (room_index, (room_position, o)) in self
-			.rooms
-			.iter()
-			.enumerate()
-			.filter(|(room_index, room)| !match room_index {
-				0 => room
-					.iter()
-					.all(|&c| c == Occupant::A || c == Occupant::Empty),
-				1 => room
-					.iter()
-					.all(|&c| c == Occupant::B || c == Occupant::Empty),
-				2 => room
-					.iter()
-					.all(|&c| c == Occupant::C || c == Occupant::Empty),
-				3 => room
-					.iter()
-					.all(|&c| c == Occupant::D || c == Occupant::Empty),
-				_ => unreachable!(),
-			})
-			.map(|(room_index, room)| {
-				(
-					room_index,
-					room.iter().find_position(|&c| c != &Occupant::Empty),
-				)
-			})
-			.filter(|(_, o)| o.is_some())
-			.map(|(room_index, o)| (room_index, o.unwrap()))
-		{
-			for index in [0, 1, 3, 5, 7, 9, 10] {
-				let ri = 2 + room_index * 2;
-				let (i1, i2) = (ri.min(index), ri.max(index));
-				let can_move = self.hallway[i1..=i2].iter().all(|&c| c == Occupant::Empty);
+		// Attempt to move room Amphipods
+		for room_id in 0..4 {
+			if let Some((amphipod, room_position, exit_cost)) = self.rooms[room_id].get_next() {
+				let room_index = 2 * room_id + 2;
 
-				if can_move {
-					let move_cost = i2 - i1 + room_position + 1;
+				// Try see if they can move directly to their destination
+				// but fall back on just into the hallway instead.
+				if self.rooms[amphipod.destination()].accepts(amphipod) {
+					if let Some(hallway_cost) = self.cost_to_move(room_index, amphipod) {
+						let mut next_state = *self;
+						let room_cost = next_state.rooms[amphipod.destination()].insert(amphipod);
+						next_state.rooms[room_id].cells[room_position] = None;
+						next_state.cost += hallway_cost + room_cost + exit_cost;
+						moves.push(next_state);
+					}
+				} else {
+					// for destination_index in [10] {
+					for destination_index in [0, 1, 3, 5, 7, 9, 10] {
+						if let Some(hallway_cost) =
+							self.cost_to_move_to_position(room_index, destination_index, amphipod)
+						{
+							let mut next_state = *self;
 
-					let mut new_state = self.clone();
-					new_state.cost = Reverse(
-						self.cost.0
-							+ match o {
-								Occupant::A => move_cost,
-								Occupant::B => move_cost * 10,
-								Occupant::C => move_cost * 100,
-								Occupant::D => move_cost * 1000,
-								_ => unreachable!(),
-							},
-					);
-					new_state.hallway[index] = *o;
-					new_state.rooms[room_index][room_position] = Occupant::Empty;
+							next_state.hallway[destination_index] = Some(amphipod);
+							next_state.rooms[room_id].cells[room_position] = None;
+							next_state.cost += hallway_cost + exit_cost;
 
-					moves.push(new_state);
+							moves.push(next_state);
+						}
+					}
 				}
 			}
 		}
 
 		moves
+	}
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+struct Room<const N: usize> {
+	id: Amphipod,
+	cells: [Option<Amphipod>; N],
+}
+
+impl<const N: usize> Room<N> {
+	fn is_solved(&self) -> bool {
+		self.cells.iter().all(|cell| cell == &Some(self.id))
+	}
+
+	fn accepts(&self, amphipod: Amphipod) -> bool {
+		self.id == amphipod
+			&& self
+				.cells
+				.iter()
+				.all(|&cell| cell.is_none() || cell == Some(self.id))
+	}
+
+	fn insert(&mut self, amphipod: Amphipod) -> usize {
+		self.cells
+			.iter_mut()
+			.enumerate()
+			.rev()
+			.find(|(_, cell)| cell.is_none())
+			.map(|(index, cell)| {
+				*cell = Some(amphipod);
+
+				(index + 1) * amphipod.cost()
+			})
+			.unwrap()
+	}
+
+	fn get_next(&self) -> Option<(Amphipod, usize, usize)> {
+		if let Some((position, Some(amphipod))) =
+			self.cells.iter().find_position(|cell| cell.is_some())
+		{
+			let exit_cost = (position + 1) * amphipod.cost();
+
+			if self
+				.cells
+				.iter()
+				.skip(position)
+				.all(|&cell| cell == Some(self.id))
+			{
+				return None;
+			}
+
+			return Some((*amphipod, position, exit_cost));
+		}
+
+		None
 	}
 }
 
@@ -317,3 +327,153 @@ advent::problem!(
 	12521,
 	44169,
 );
+
+#[cfg(test)]
+mod addtional_tests {
+	use super::*;
+
+	fn make_input(input: &str) -> String {
+		let mut s = String::from("\n\n");
+		s.push_str(&input[0..4]);
+		s.push('\n');
+		s.push_str(&input[4..8]);
+
+		s
+	}
+
+	#[test]
+	fn states_can_be_determined_if_solved() {
+		let a = parse::<2>(&make_input("ABCDABCD"));
+		let b = parse::<2>(&make_input("AACDBBCD"));
+
+		assert!(a.is_solved());
+		assert!(!b.is_solved());
+	}
+
+	#[test]
+	fn rooms_can_insert_amphipods() {
+		let mut room = Room {
+			id: Amphipod::Amber,
+			cells: [None, None],
+		};
+
+		assert!(room.accepts(Amphipod::Amber));
+		assert!(!room.accepts(Amphipod::Bronze));
+		assert!(!room.accepts(Amphipod::Copper));
+		assert!(!room.accepts(Amphipod::Desert));
+		assert_eq!(2, room.insert(Amphipod::Amber));
+		assert_eq!([None, Some(Amphipod::Amber)], room.cells);
+		assert_eq!(1, room.insert(Amphipod::Amber));
+		assert_eq!([Some(Amphipod::Amber), Some(Amphipod::Amber)], room.cells);
+	}
+
+	#[test]
+	fn rooms_only_accept_if_they_have_been_emptied_of_invalid_ones() {
+		let room = Room {
+			id: Amphipod::Bronze,
+			cells: [None, None, Some(Amphipod::Bronze), Some(Amphipod::Desert)],
+		};
+
+		assert!(!room.accepts(Amphipod::Amber));
+		assert!(!room.accepts(Amphipod::Bronze));
+		assert!(!room.accepts(Amphipod::Copper));
+		assert!(!room.accepts(Amphipod::Desert));
+	}
+
+	#[test]
+	fn amphipods_can_move_to_a_room() {
+		let state = parse::<2>(&make_input("ABCDABCD"));
+
+		for (amphipod, index, expected_cost) in [
+			(Amphipod::Amber, 0, 2),
+			(Amphipod::Amber, 10, 8),
+			(Amphipod::Bronze, 0, 40),
+			(Amphipod::Bronze, 10, 60),
+			(Amphipod::Copper, 0, 600),
+			(Amphipod::Copper, 10, 400),
+			(Amphipod::Desert, 0, 8000),
+			(Amphipod::Desert, 10, 2000),
+		] {
+			let mut state = state.clone();
+			state.hallway[index] = Some(amphipod);
+
+			assert_eq!(Some(expected_cost), state.cost_to_move(index, amphipod));
+		}
+	}
+
+	#[test]
+	fn a_blocked_hallway_returns_none_for_cost() {
+		let state = parse::<2>(&make_input("ABCDABCD"));
+
+		for (amphipod, index) in [
+			(Amphipod::Amber, 10),
+			(Amphipod::Bronze, 10),
+			(Amphipod::Copper, 0),
+			(Amphipod::Desert, 0),
+		] {
+			let mut state = state.clone();
+			state.hallway[index] = Some(amphipod);
+			state.hallway[5] = Some(amphipod);
+
+			assert_eq!(None, state.cost_to_move(index, amphipod));
+		}
+	}
+
+	#[test]
+	fn a_room_can_say_what_amphipod_would_leave_it_next() {
+		let mut state = parse::<2>(&make_input("ABCDDACB"));
+		state.rooms[2].cells[0] = None;
+		state.rooms[3].cells[0] = None;
+
+		for (index, expected) in [
+			(0, Some((Amphipod::Amber, 0, 1))),
+			(1, Some((Amphipod::Bronze, 0, 10))),
+			(2, None),
+			(3, Some((Amphipod::Bronze, 1, 20))),
+		] {
+			assert_eq!(expected, state.rooms[index].get_next());
+		}
+	}
+
+	#[test]
+	fn amphipods_in_their_destination_will_leave_if_they_are_blocking_others() {
+		let state = parse::<2>(&make_input("ABCDBCDA"));
+
+		for (index, expected) in [
+			(0, Some((Amphipod::Amber, 0, 1))),
+			(1, Some((Amphipod::Bronze, 0, 10))),
+			(2, Some((Amphipod::Copper, 0, 100))),
+			(3, Some((Amphipod::Desert, 0, 1000))),
+		] {
+			assert_eq!(expected, state.rooms[index].get_next());
+		}
+	}
+
+	#[test]
+	fn stuck_state_move_test() {
+		let mut state = parse::<2>(&make_input("ABCDABCA"));
+		state.rooms[0].cells[0] = None;
+		state.hallway[10] = Some(Amphipod::Desert);
+
+		let moves = state.next_moves();
+
+		assert_eq!(6, moves.len());
+	}
+
+	#[test]
+	fn states_are_equivalent_based_on_the_hallway_and_rooms_but_not_cost() {
+		let mut a = parse::<2>(&make_input("ABCDABCD"));
+		let mut b = parse::<2>(&make_input("ABCDABCD"));
+		assert_eq!(a, b);
+
+		a.cost = 10;
+		b.cost = 20;
+		assert_eq!(a, b);
+
+		a.hallway[0] = Some(Amphipod::Bronze);
+		a.hallway[1] = None;
+		b.hallway[0] = None;
+		b.hallway[1] = Some(Amphipod::Bronze);
+		assert_ne!(a, b);
+	}
+}
