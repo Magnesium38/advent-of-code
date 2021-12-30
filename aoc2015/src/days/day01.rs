df3a67c2826468047dc36e917fd5797d02ae0087
Eric Jensen <magnesium38@gmail.com>
2021-12-30 13:31:40 -0600

    2015 day 1

--- aoc2015/src/days/day01.rs
+++ aoc2015/src/days/day01.rs
@@ -1,15 +1,41 @@
-pub fn pt1(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
+use itertools::Itertools;
+
+pub fn pt1(input: &str) -> anyhow::Result<isize> {
+	Ok(input.chars().fold(0, |total, c| {
+		total
+			+ match c {
+				'(' => 1,
+				')' => -1,
+				_ => unreachable!(),
+			}
+	}))
 }
 
-pub fn pt2(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
-}
+pub fn pt2(input: &str) -> anyhow::Result<isize> {
+	Ok(input
+		.chars()
+		.zip(1..)
+		.fold_while(0, |total, (c, position)| {
+			let new_total = total
+				+ match c {
+					'(' => 1,
+					')' => -1,
+					_ => unreachable!(),
+				};
 
+			if new_total < 0 {
+				itertools::FoldWhile::Done(position)
+			} else {
+				itertools::FoldWhile::Continue(new_total)
+			}
+		})
+		.into_inner())
+}
 
 advent::problem!(
 	r#"
+		()())
     "#,
-	0,
-	0,
+	-1,
+	5,
 );
