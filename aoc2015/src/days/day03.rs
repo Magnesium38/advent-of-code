059ae51ec0fbb53bbf43a87eaf0b40af88d8f06c
Eric Jensen <magnesium38@gmail.com>
2021-12-30 14:10:11 -0600

    2015 day 3

--- aoc2015/src/days/day03.rs
+++ aoc2015/src/days/day03.rs
@@ -1,15 +1,58 @@
-pub fn pt1(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
+use hashbrown::HashMap;
+
+pub fn pt1(input: &str) -> anyhow::Result<usize> {
+	let mut grid: HashMap<(isize, isize), usize> = HashMap::new();
+	let mut position = (0, 0);
+
+	grid.insert((0, 0), 1);
+	input.chars().for_each(|c| {
+		match c {
+			'^' => position.1 -= 1,
+			'v' => position.1 += 1,
+			'<' => position.0 -= 1,
+			'>' => position.0 += 1,
+			_ => unreachable!(),
+		};
+
+		*grid.entry(position).or_insert(0) += 1;
+	});
+
+	Ok(grid.len())
 }
 
-pub fn pt2(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
-}
+pub fn pt2(input: &str) -> anyhow::Result<usize> {
+	let mut grid: HashMap<(isize, isize), usize> = HashMap::new();
+	let mut santa_position = (0, 0);
+	let mut robo_position = (0, 0);
 
+	grid.insert((0, 0), 1);
+	input.chars().enumerate().for_each(|(i, c)| {
+		match (i % 2, c) {
+			(0, '^') => santa_position.1 -= 1,
+			(0, 'v') => santa_position.1 += 1,
+			(0, '<') => santa_position.0 -= 1,
+			(0, '>') => santa_position.0 += 1,
+			(1, '^') => robo_position.1 -= 1,
+			(1, 'v') => robo_position.1 += 1,
+			(1, '<') => robo_position.0 -= 1,
+			(1, '>') => robo_position.0 += 1,
+			_ => unreachable!(),
+		};
+
+		if i % 2 == 0 {
+			*grid.entry(santa_position).or_insert(0) += 1;
+		} else {
+			*grid.entry(robo_position).or_insert(0) += 1;
+		}
+	});
+
+	Ok(grid.len())
+}
 
 advent::problem!(
 	r#"
+		^v^v^v^v^v
     "#,
-	0,
-	0,
+	2,
+	11,
 );
