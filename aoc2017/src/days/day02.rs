e194a06fcfbabe36e9a2560dcba408ad9eb659eb
Eric Jensen <magnesium38@gmail.com>
2022-01-02 15:40:42 -0600

    2017 day 2

--- aoc2017/src/days/day02.rs
+++ aoc2017/src/days/day02.rs
@@ -1,15 +1,44 @@
-pub fn pt1(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
+use itertools::{Itertools, MinMaxResult};
+
+pub fn pt1(input: &str) -> anyhow::Result<isize> {
+	Ok(input
+		.lines()
+		.map(|line| {
+			if let MinMaxResult::MinMax(min, max) = line
+				.split_whitespace()
+				.map(|s| s.parse::<isize>().unwrap())
+				.minmax()
+			{
+				max - min
+			} else {
+				unreachable!()
+			}
+		})
+		.sum())
 }
 
-pub fn pt2(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
-}
+pub fn pt2(input: &str) -> anyhow::Result<isize> {
+	Ok(input
+		.lines()
+		.map(|line| {
+			let (a, b) = line
+				.split_whitespace()
+				.map(|s| s.parse::<isize>().unwrap())
+				.tuple_combinations()
+				.find(|(a, b)| a.max(b) % b.min(a) == 0)
+				.unwrap();
 
+			a.max(b) / b.min(a)
+		})
+		.sum())
+}
 
 advent::problem!(
 	r#"
+		5 9 2 8
+		9 4 7 3
+		3 8 6 5
     "#,
-	0,
-	0,
+	18,
+	9,
 );
