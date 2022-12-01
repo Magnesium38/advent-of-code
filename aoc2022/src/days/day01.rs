9cb282d697d3bac87f454bef5542350a482eede4
Eric Jensen <magnesium38@gmail.com>
2022-11-30 23:17:40 -0600

    2022 day 1

--- aoc2022/src/days/day01.rs
+++ aoc2022/src/days/day01.rs
@@ -1,15 +1,48 @@
-pub fn pt1(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
+use itertools::Itertools;
+
+pub fn pt1(input: &str) -> anyhow::Result<isize> {
+	Ok(input
+		.lines()
+		.map(|s| s.parse::<isize>())
+		.group_by(|s| s.is_ok())
+		.into_iter()
+		.map(|group| group.1.map(|el| el.unwrap_or(0)))
+		.map(|group| group.sum::<isize>())
+		.max()
+		.unwrap())
 }
 
-pub fn pt2(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
+pub fn pt2(input: &str) -> anyhow::Result<isize> {
+	Ok(input
+		.lines()
+		.map(|s| s.parse::<isize>())
+		.group_by(|s| s.is_ok())
+		.into_iter()
+		.map(|group| group.1.map(|el| el.unwrap_or(0)))
+		.map(|group| group.sum::<isize>())
+		.sorted()
+		.rev()
+		.take(3)
+		.sum())
 }
-
 
 advent::problem!(
 	r#"
+		1000
+		2000
+		3000
+
+		4000
+
+		5000
+		6000
+
+		7000
+		8000
+		9000
+
+		10000
     "#,
-	0,
-	0,
+	24000,
+	45000,
 );
