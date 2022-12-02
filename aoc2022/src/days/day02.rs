9ac3a8693876d4c43936ca0c6d7aee0f08b3cce0
Eric Jensen <magnesium38@gmail.com>
2022-12-01 23:39:06 -0600

    simplify scoring

--- aoc2022/src/days/day02.rs
+++ aoc2022/src/days/day02.rs
@@ -1,39 +1,28 @@
-use std::ops::Sub;
-
 use itertools::Itertools;
 
-#[derive(PartialEq, Copy, Clone)]
+#[derive(Clone, Copy)]
 enum Rps {
 	Rock,
 	Paper,
 	Scissors,
 }
 
-impl Sub for Rps {
-	type Output = isize;
-
-	fn sub(self, rhs: Self) -> Self::Output {
-		match (self, rhs) {
-			(Rps::Rock, Rps::Rock) => 3,
-			(Rps::Rock, Rps::Scissors) => 0,
-			(Rps::Rock, Rps::Paper) => 6,
-			(Rps::Paper, Rps::Rock) => 0,
-			(Rps::Paper, Rps::Scissors) => 6,
-			(Rps::Paper, Rps::Paper) => 3,
-			(Rps::Scissors, Rps::Rock) => 6,
-			(Rps::Scissors, Rps::Scissors) => 3,
-			(Rps::Scissors, Rps::Paper) => 0,
-		}
-	}
-}
-
 fn score((opponent, player): (Rps, Rps)) -> isize {
-	(opponent - player)
-		+ match player {
-			Rps::Rock => 1,
-			Rps::Paper => 2,
-			Rps::Scissors => 3,
-		}
+	(match player {
+		Rps::Rock => 1,
+		Rps::Paper => 2,
+		Rps::Scissors => 3,
+	}) + (match (opponent, player) {
+		(Rps::Rock, Rps::Rock) => 3,
+		(Rps::Rock, Rps::Scissors) => 0,
+		(Rps::Rock, Rps::Paper) => 6,
+		(Rps::Paper, Rps::Rock) => 0,
+		(Rps::Paper, Rps::Scissors) => 6,
+		(Rps::Paper, Rps::Paper) => 3,
+		(Rps::Scissors, Rps::Rock) => 6,
+		(Rps::Scissors, Rps::Scissors) => 3,
+		(Rps::Scissors, Rps::Paper) => 0,
+	})
 }
 
 pub fn pt1(input: &str) -> anyhow::Result<isize> {
