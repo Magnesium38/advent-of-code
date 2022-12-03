33c90b0a1ce32d653a8258be656a99627519f456
Eric Jensen <magnesium38@gmail.com>
2022-12-03 00:07:47 -0600

    clean up bit solution

--- aoc2022/src/days/day03.rs
+++ aoc2022/src/days/day03.rs
@@ -1,18 +1,21 @@
 use itertools::Itertools;
 
-fn temp(s: &str) -> u64 {
-	let mut n = 0;
-
-	for c in s.as_bytes() {
-		n |= 1
-			<< match c {
-				b'a'..=b'z' => (c - b'a') + 1,
-				b'A'..=b'Z' => (c - b'A') + 27,
+fn encode(s: &str) -> u64 {
+	s.as_bytes().iter().fold(0u64, |n, el| {
+		n | 1
+			<< match el {
+				b'a'..=b'z' => (el - b'a') + 1,
+				b'A'..=b'Z' => (el - b'A') + 27,
 				_ => unreachable!(),
-			};
-	}
+			}
+	})
+}
 
-	n
+fn find_common<'a, I: Iterator<Item = &'a str>>(iter: I) -> u32 {
+	iter.map(encode)
+		.reduce(|acc, el| acc & el)
+		.unwrap()
+		.trailing_zeros()
 }
 
 pub fn pt1(input: &str) -> anyhow::Result<u32> {
@@ -21,22 +24,13 @@ pub fn pt1(input: &str) -> anyhow::Result<u32> {
 		.map(|line| {
 			let (a, b) = line.split_at(line.len() / 2);
 
-			(temp(a) & temp(b)).trailing_zeros()
+			find_common([a, b].iter().copied())
 		})
 		.sum())
 }
 
 pub fn pt2(input: &str) -> anyhow::Result<u32> {
-	Ok(input
-		.lines()
-		.chunks(3)
-		.into_iter()
-		.map(|chunk| {
-			let (a, b, c) = chunk.collect_tuple().unwrap();
-
-			(temp(a) & temp(b) & temp(c)).trailing_zeros()
-		})
-		.sum())
+	Ok(input.lines().chunks(3).into_iter().map(find_common).sum())
 }
 
 advent::problem!(
