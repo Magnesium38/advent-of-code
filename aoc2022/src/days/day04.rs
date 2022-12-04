8de66bbe9acfbb04902c10b11294eec0a69206ed
Eric Jensen <magnesium38@gmail.com>
2022-12-03 23:16:54 -0600

    simplify parsing

--- aoc2022/src/days/day04.rs
+++ aoc2022/src/days/day04.rs
@@ -4,29 +4,19 @@ pub fn pt1(input: &str) -> anyhow::Result<usize> {
 	Ok(input
 		.lines()
 		.map(|line| {
-			let (a, b) = line.split(',').collect_tuple().unwrap();
-
-			let (a1, a2) = a
-				.split('-')
-				.map(|n| n.parse::<usize>())
+			line.split([',', '-'])
+				.map(|n| n.parse::<usize>().expect("unable to parse input"))
 				.collect_tuple()
-				.unwrap();
-			let (b1, b2) = b
-				.split('-')
-				.map(|n| n.parse::<usize>())
-				.collect_tuple()
-				.unwrap();
-
-			((a1.unwrap(), a2.unwrap()), (b1.unwrap(), b2.unwrap()))
+				.expect("expected four elements")
 		})
-		.map(|((a1, a2), (b1, b2))| {
+		.map(|(a1, a2, b1, b2)| {
 			if b2 - b1 > a2 - a1 {
-				((b1, b2), (a1, a2))
+				(b1, b2, a1, a2)
 			} else {
-				((a1, a2), (b1, b2))
+				(a1, a2, b1, b2)
 			}
 		})
-		.filter(|((a1, a2), (b1, b2))| a1 <= b1 && a2 >= b2)
+		.filter(|(a1, a2, b1, b2)| a1 <= b1 && a2 >= b2)
 		.count())
 }
 
@@ -34,22 +24,12 @@ pub fn pt2(input: &str) -> anyhow::Result<usize> {
 	Ok(input
 		.lines()
 		.map(|line| {
-			let (a, b) = line.split(',').collect_tuple().unwrap();
-
-			let (a1, a2) = a
-				.split('-')
-				.map(|n| n.parse::<usize>())
+			line.split([',', '-'])
+				.map(|n| n.parse::<usize>().expect("unable to parse input"))
 				.collect_tuple()
-				.unwrap();
-			let (b1, b2) = b
-				.split('-')
-				.map(|n| n.parse::<usize>())
-				.collect_tuple()
-				.unwrap();
-
-			((a1.unwrap(), a2.unwrap()), (b1.unwrap(), b2.unwrap()))
+				.expect("expected four elements")
 		})
-		.filter(|((a1, a2), (b1, b2))| {
+		.filter(|(a1, a2, b1, b2)| {
 			(a1 <= b1 && a2 >= b1)
 				|| (a1 <= b2 && a2 >= b2)
 				|| (b1 <= a1 && b2 >= a1)
