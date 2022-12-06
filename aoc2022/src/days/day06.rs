728b847415b76fd20fbb72adcc7079b31ad25206
Eric Jensen <magnesium38@gmail.com>
2022-12-05 23:15:14 -0600

    initial day 6

--- aoc2022/src/days/day06.rs
+++ aoc2022/src/days/day06.rs
@@ -1,15 +1,35 @@
-pub fn pt1(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
+pub fn pt1(input: &str) -> anyhow::Result<usize> {
+	Ok(input
+		.chars()
+		.enumerate()
+		.tuple_windows()
+		.map(|((i, a), (_, b), (_, c), (_, d))| -> (_, HashSet<_>) {
+			(i + 4, HashSet::from_iter([a, b, c, d]))
+		})
+		.find(|(_, set)| set.len() == 4)
+		.unwrap()
+		.0)
 }
 
-pub fn pt2(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
+pub fn pt2(input: &str) -> anyhow::Result<usize> {
+	Ok(input
+		.as_bytes()
+		.windows(14)
+		.map(|window| -> HashSet<_> { HashSet::from_iter(window) })
+		.enumerate()
+		.find(|(_, set)| set.len() == 14)
+		.unwrap()
+		.0 + 14)
 }
-
 
 advent::problem!(
 	r#"
+		mjqjpqmgbljsphdztnvjfqwrcgsmlb
     "#,
-	0,
-	0,
+	7,
+	19,
 );
