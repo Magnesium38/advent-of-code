fa7722cf900b596bab29e78f99f4c58e2d8c2299
Eric Jensen <magnesium38@gmail.com>
2022-12-07 12:09:49 -0600

    rough first pass refactor into a one pass iterator solution

--- aoc2022/src/days/day07.rs
+++ aoc2022/src/days/day07.rs
@@ -1,111 +1,43 @@
-use hashbrown::HashMap;
 use itertools::Itertools;
-use std::path::PathBuf;
 
-#[derive(Debug, Clone)]
-struct Directory {
-	path: PathBuf,
-	files: Vec<usize>,
-	directories: Vec<PathBuf>,
-}
+fn parse<'a>(input: &mut impl Iterator<Item = &'a str>) -> Vec<usize> {
+	let mut sizes = vec![];
+	let mut total = 0;
 
-impl Directory {
-	fn new(path: PathBuf) -> Self {
-		Directory {
-			path,
-			files: Vec::new(),
-			directories: Vec::new(),
-		}
-	}
-
-	fn get_size(&self, fs: &FileSystem) -> usize {
-		self.files.iter().sum::<usize>()
-			+ self
-				.directories
-				.iter()
-				.map(|path| {
-					fs.directories
-						.get(&self.path.with_file_name(path))
-						.unwrap()
-						.get_size(fs)
-				})
-				.sum::<usize>()
-	}
-}
-
-#[derive(Debug, Clone)]
-struct FileSystem {
-	directories: HashMap<PathBuf, Directory>,
-	working_directory: PathBuf,
-}
-
-impl FileSystem {
-	fn new() -> Self {
-		FileSystem {
-			directories: HashMap::new(),
-			working_directory: PathBuf::new(),
-		}
-	}
-
-	fn from_input(input: &str) -> Self {
-		input.lines().fold(FileSystem::new(), |mut fs, line| {
-			if line.starts_with('$') {
-				if line.starts_with("$ ls") {
-					// Nothing
-				} else if line == "$ cd .." {
-					fs.working_directory.pop();
-				} else if line == "$ cd /" {
-					fs.working_directory = PathBuf::from("/");
-				} else {
-					fs.working_directory = fs.working_directory.join(&line[5..]);
-				}
-			} else if line.starts_with("dir") {
-				fs.directories
-					.entry(fs.working_directory.clone())
-					.or_insert(Directory::new(fs.working_directory.clone()))
-					.directories
-					.push(fs.working_directory.clone().join(&line[4..]));
-			} else {
-				let (size, _) = line.split_once(' ').unwrap();
-
-				fs.directories
-					.entry(fs.working_directory.clone())
-					.or_insert(Directory::new(fs.working_directory.clone()))
-					.files
-					.push(size.parse().unwrap());
+	loop {
+		match input
+			.next()
+			.map(|line| line.split(' ').collect_vec())
+			.as_deref()
+		{
+			Some(["$", "ls"]) | Some(["dir", _]) => {}
+			Some(["$", "cd", ".."]) | None => break,
+			Some(["$", "cd", _]) => {
+				sizes.extend(parse(input));
+				total += sizes.last().unwrap();
 			}
-
-			fs
-		})
+			Some([size, _]) => total += size.parse::<usize>().unwrap(),
+			_ => unreachable!(),
+		}
 	}
+
+	sizes.push(total);
+	sizes
 }
 
 pub fn pt1(input: &str) -> anyhow::Result<usize> {
-	let fs = FileSystem::from_input(input);
-
-	Ok(fs
-		.directories
-		.iter()
-		.map(|(_, directory)| directory.get_size(&fs))
+	Ok(parse(&mut input.lines())
+		.into_iter()
 		.filter(|size| *size < 100000)
 		.sum())
 }
 
 pub fn pt2(input: &str) -> anyhow::Result<usize> {
-	let fs = FileSystem::from_input(input);
+	let sizes = parse(&mut input.lines());
+	let minimum = 30000000 - (70000000 - sizes.iter().max().unwrap());
 
-	let used_space = fs
-		.directories
-		.get(&PathBuf::from("/"))
-		.unwrap()
-		.get_size(&fs);
-
-	let minimum = 30000000 - (70000000 - used_space);
-
-	Ok(fs
-		.directories
-		.iter()
-		.map(|(_, directory)| directory.get_size(&fs))
+	Ok(sizes
+		.into_iter()
 		.sorted()
 		.find(|size| *size > minimum)
 		.unwrap())
