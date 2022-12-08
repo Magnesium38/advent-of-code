b53573b48cd6eecf239f2695e71b342ef43f89fb
Eric Jensen <magnesium38@gmail.com>
2022-12-08 00:37:05 -0600

    refactor part two visibility counting

--- aoc2022/src/days/day08.rs
+++ aoc2022/src/days/day08.rs
@@ -1,6 +1,6 @@
 use advent::Grid;
 use hashbrown::HashSet;
-use itertools::FoldWhile::{Continue, Done};
+use itertools::FoldWhile::{self, Continue, Done};
 use itertools::Itertools;
 
 fn is_visible(grid: &Grid<u32>, (x, y): (isize, isize)) -> bool {
@@ -45,56 +45,48 @@ fn is_visible(grid: &Grid<u32>, (x, y): (isize, isize)) -> bool {
 	})
 }
 
+fn fold_direction(
+	grid: &Grid<u32>,
+	main_tree: u32,
+	tree: u32,
+	count: usize,
+	x: isize,
+	y: isize,
+) -> FoldWhile<(u32, usize)> {
+	let size = *grid.get(x, y).unwrap();
+	if main_tree <= size {
+		Done((size, count + 1))
+	} else {
+		Continue((size.max(tree), count + 1))
+	}
+}
+
 fn count_visible(grid: &Grid<u32>, (x, y): (isize, isize)) -> usize {
 	let main_tree = *grid.get(x, y).unwrap();
 
-	let into_west = (0..x)
+	(0..x)
 		.rev()
-		.fold_while((0, 0), |(tree, count), i| match grid.get(i, y) {
-			Some(&size) => match main_tree <= size {
-				true => Done((size, count + 1)),
-				false => Continue((size.max(tree), count + 1)),
-			},
-			None => Done((tree, count)),
+		.fold_while((0, 0), |(tree, count), i| {
+			fold_direction(grid, main_tree, tree, count, i, y)
 		})
 		.into_inner()
-		.1;
-
-	let into_east = (x + 1..grid.width.try_into().unwrap())
-		.fold_while((0, 0), |(tree, count), i| match grid.get(i, y) {
-			Some(&size) => match main_tree <= size {
-				true => Done((size, count + 1)),
-				false => Continue((size.max(tree), count + 1)),
-			},
-			None => Done((tree, count)),
+		.1 * (x + 1..grid.width.try_into().unwrap())
+		.fold_while((0, 0), |(tree, count), i| {
+			fold_direction(grid, main_tree, tree, count, i, y)
 		})
 		.into_inner()
-		.1;
-
-	let into_north = (0..y)
+		.1 * (0..y)
 		.rev()
-		.fold_while((0, 0), |(tree, count), j| match grid.get(x, j) {
-			Some(&size) => match main_tree <= size {
-				true => Done((size, count + 1)),
-				false => Continue((size.max(tree), count + 1)),
-			},
-			None => Done((tree, count)),
+		.fold_while((0, 0), |(tree, count), j| {
+			fold_direction(grid, main_tree, tree, count, x, j)
 		})
 		.into_inner()
-		.1;
-
-	let into_south = (y + 1..grid.height.try_into().unwrap())
-		.fold_while((0, 0), |(tree, count), j| match grid.get(x, j) {
-			Some(&size) => match main_tree <= size {
-				true => Done((size, count + 1)),
-				false => Continue((size.max(tree), count + 1)),
-			},
-			None => Done((tree, count)),
+		.1 * (y + 1..grid.height.try_into().unwrap())
+		.fold_while((0, 0), |(tree, count), j| {
+			fold_direction(grid, main_tree, tree, count, x, j)
 		})
 		.into_inner()
-		.1;
-
-	into_west * into_east * into_north * into_south
+		.1
 }
 
 pub fn pt1(input: &str) -> anyhow::Result<usize> {
