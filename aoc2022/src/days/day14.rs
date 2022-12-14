81f257c0dbb9ab4cd1f06a55f21179ae80bff873
Eric Jensen <magnesium38@gmail.com>
2022-12-13 23:46:52 -0600

    day 14 - not a shabby first implementation

--- aoc2022/src/days/day14.rs
+++ aoc2022/src/days/day14.rs
@@ -1,15 +1,131 @@
-pub fn pt1(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
+use hashbrown::HashMap;
+use itertools::Itertools;
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+struct Coordinate {
+	x: usize,
+	y: usize,
 }
 
-pub fn pt2(_input: &str) -> anyhow::Result<isize> {
-	Ok(0)
+impl From<&str> for Coordinate {
+	fn from(input: &str) -> Self {
+		let (x, y) = input.split_once(',').unwrap();
+
+		Self {
+			x: x.parse().unwrap(),
+			y: y.parse().unwrap(),
+		}
+	}
 }
 
+impl Coordinate {
+	fn new(x: usize, y: usize) -> Self {
+		Self { x, y }
+	}
+}
+
+#[derive(Debug)]
+enum Node {
+	Rock,
+	Sand,
+}
+
+fn parse_grid(input: &str) -> (HashMap<Coordinate, Node>, usize) {
+	let mut grid = HashMap::new();
+
+	let mut max_y = 0;
+
+	input.lines().for_each(|line| {
+		line.split(" -> ")
+			.map(Coordinate::from)
+			.tuple_windows()
+			.for_each(|(p1, p2)| {
+				for x in p1.x.min(p2.x)..=p1.x.max(p2.x) {
+					for y in p1.y.min(p2.y)..=p1.y.max(p2.y) {
+						grid.insert(Coordinate::new(x, y), Node::Rock);
+					}
+				}
+
+				max_y = max_y.max(p1.y).max(p2.y);
+			});
+	});
+
+	(grid, max_y)
+}
+
+fn drop_sand(
+	grid: &mut HashMap<Coordinate, Node>,
+	max_y: usize,
+	mut coords: Coordinate,
+) -> Option<Coordinate> {
+	for y in coords.y..=max_y {
+		coords.y = y;
+
+		if let Some(_) = grid.get(&coords) {
+			if coords.y == 0 {
+				return None;
+			}
+
+			coords.x -= 1;
+			if let Some(_) = grid.get(&coords) {
+				coords.x += 2;
+				if let Some(_) = grid.get(&coords) {
+					coords.x -= 1;
+					coords.y -= 1;
+
+					grid.insert(coords, Node::Sand);
+
+					return Some(coords);
+				}
+			}
+
+			return drop_sand(grid, max_y, coords);
+		}
+	}
+
+	None
+}
+
+pub fn pt1(input: &str) -> anyhow::Result<usize> {
+	let (mut grid, max_y) = parse_grid(input);
+
+	loop {
+		if matches!(drop_sand(&mut grid, max_y, Coordinate::new(500, 0)), None) {
+			break;
+		}
+	}
+
+	Ok(grid
+		.values()
+		.filter(|node| matches!(node, Node::Sand))
+		.count())
+}
+
+pub fn pt2(input: &str) -> anyhow::Result<usize> {
+	let (mut grid, mut max_y) = parse_grid(input);
+	max_y += 2;
+
+	for x in (500 - (2 * max_y))..=(500 + 2 * max_y) {
+		grid.insert(Coordinate::new(x, max_y), Node::Rock);
+	}
+
+	loop {
+		if matches!(drop_sand(&mut grid, max_y, Coordinate::new(500, 0)), None) {
+			break;
+		}
+	}
+
+	Ok(grid
+		.values()
+		.filter(|node| matches!(node, Node::Sand))
+		.count())
+}
 
 advent::problem!(
 	r#"
+		498,4 -> 498,6 -> 496,6
+		503,4 -> 502,4 -> 502,9 -> 494,9
     "#,
-	0,
-	0,
+	24,
+	93,
 );
